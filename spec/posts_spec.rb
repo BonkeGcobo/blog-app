6d50f99ed09b0a630f73a5688b4658df5da923e9
BonkeG <madixmicro@gmail.com>
2022-06-10 19:24:50 +0200

    Add posts tests

---
+++ spec/posts_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe 'Posts', type: :request do
+  before(:example) { get user_posts_path({user_id: 2}) }
+  it "is a success" do
+    expect(response).to have_http_status(:ok)
+  end
+
+  it "Tests if the correct page will be rendered" do
+    expect(response).to render_template(:index)
+  end
+
+  it "Test if the user posts are present" do
+    expect(response.body).to include('Here are user posts') 
+  end
+end
+
+describe 'Posts', type: :request do
+  before(:example) {get user_post_path({user_id:2, id:6})} 
+  it "is a success" do
+    expect(response).to have_http_status(:ok)
+  end
+
+  it "Tests if the correct page will be rendered" do
+    expect(response).to render_template(:show)
+  end
+
+  it "Test if the user posts are present" do
+    expect(response.body).to include('Here is post by user') 
+  end
+end
