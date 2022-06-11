5b9ad1d8b0f014922f73e66724d67504ba26ccf5
BonkeG <madixmicro@gmail.com>
2022-06-11 17:28:23 +0200

    fix table errors

--- db/migrate/20220611152416_create_like.rb
+++ db/migrate/20220611152416_create_like.rb
@@ -1,9 +1,8 @@
-class CreateComment < ActiveRecord::Migration[7.0]
+class CreateLike < ActiveRecord::Migration[7.0]
   def change
-    create_table :comments do |t|
+    create_table :likes do |t|
       t.references :Author, index: true, foreign_key: {to_table: :users}
       t.references :Post, index: true, foreign_key: {to_table: :posts}
-      t.text :Text
       t.timestamps
     end
   end
