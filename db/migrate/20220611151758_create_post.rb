e0151127901d55d80b60f7e41a63d142cc1d757c
BonkeG <madixmicro@gmail.com>
2022-06-11 17:19:51 +0200

    add user post tables

---
+++ db/migrate/20220611151758_create_post.rb
@@ -0,0 +1,13 @@
+class CreatePost < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.references :Author, index: true, foreign_key: {to_table: :user}
+      t.string :Title, :limit => 32
+      t.text :Text
+      t.timestamps
+      t.bigint :CommentsCounter
+      t.bigint :LikesCounter
+      t.timestamps
+    end
+  end
+end
