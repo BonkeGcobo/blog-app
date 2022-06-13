372f8a618ad3b9b76982e8a9c399b0c981e40acd
BonkeG <madixmicro@gmail.com>
2022-06-13 16:16:07 +0200

    change has_one :post to has_many :posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :comments
-  has_many :users :comments
+  has_many :comments #Post can have 0 or more comments
+  has_many :users :comments #A posts has 0 or more users commenting on it
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  has_one :post
-  has_many :comments: :post
+  has_many :posts #user can have many posts
+  has_many :comments: :post #User can have 0 or more comments in one post
 end
