79dfec706270ea8c7d545cb84bdfa64fe550d646
BonkeG <madixmicro@gmail.com>
2022-06-13 16:23:10 +0200

    Create User-like & Post-like associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments #Post can have 0 or more comments
+  has_many :likes #Posts can have 0 or more likes
+  has_many :users :likes #Posts can have 0 or more likes from users
   has_many :users :comments #A posts has 0 or more users commenting on it
 end
