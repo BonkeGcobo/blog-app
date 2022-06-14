1e8e1563fffbc8c652cb3b646bfd743346ebbe41
BonkeG <madixmicro@gmail.com>
2022-06-14 17:15:27 +0200

    user has likes and remove posts has users

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments # Post can have 0 or more comments
   has_many :likes # Posts can have 0 or more likes
-  has_many :users, through: :likes # Posts can have 0 or more likes from users
-  has_many :users, through: :comments # A posts has 0 or more users commenting on it
-
+  
   def posts_counter
     user.update(PostsCounter: user.posts.count)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :posts # user can have many posts
   has_many :comments, through: :post # User can have 0 or more comments in one
+  has_many :likes, through: :post
 
   def first_three_posts
     posts.includes(:user).order(created_at: :DESC).limit(3)
