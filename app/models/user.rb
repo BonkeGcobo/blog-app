d21900172e4bfdedf22df6b504dbfbde49b3932c
BonkeG <madixmicro@gmail.com>
2022-06-14 12:55:10 +0200

    add method to return first three

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   has_many :posts #user can have many posts
-  has_many :comments: :post #User can have 0 or more comments in one post
+  has_many :comments, through: :post #User can have 0 or more comments in one
+
+  def first_three_posts
+    posts.includes(:user).order(created_at: :DESC).limit(3)
+  end
+    
 end
