91dbd5ac9bf2e6c529605b2ca732ea7df7a676bd
BonkeG <madixmicro@gmail.com>
2022-06-21 11:52:58 +0200

    Add route for new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:index, :show] do
+  resources :users, only: [:index, :show, :new] do
     resources :posts,  only: [:index, :show]
   end
   # Defines the root path route ("/")
