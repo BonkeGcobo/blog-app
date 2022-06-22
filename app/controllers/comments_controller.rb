ad5a2c82fc439879c2aa574b6112880350ba8402
BonkeG <madixmicro@gmail.com>
2022-06-22 16:19:07 +0200

    Allow current user to create comment

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,23 @@
+class CommentsController < ApplicationController
+  def new
+    respond_to do |format|
+      format.html { render :new, locals: { comment: Comment.new } }
+    end
+  end
+
+  def create
+    user = current_user
+    post_id = params[:post_id]
+    post = Post.find_by(id: post_id.to_i) 
+    params.permit!
+    comment = Comment.new(params[:comment])
+    comment.user = user
+    comment.post = post
+
+    if comment.save
+      flash[:notice] = 'Post saved successfully'
+      comment.update_comments_counter
+      redirect_to user_post_url(id:post.id)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'comment/new'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show] do
-    resources :posts,  only: [:index, :show, :new,:create]
+    resources :posts,  only: [:index, :show, :new,:create] do
+      resources :comments, only: [:new, :create]
+    end
   end
   # Defines the root path route ("/")
   root 'users#index'
