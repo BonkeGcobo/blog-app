9d56be236ad9e48510f6b96dd789e77ddb643129
BonkeG <madixmicro@gmail.com>
2022-06-23 17:07:50 +0200

    write test for name to be validate

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  subject { User.new(name: "Bonke")}
+  before {subject.save}
+
+  it "user name should be present" do
+    subject.name = nil
+    expect(subject).to_not be_vaild
+  end
 end
