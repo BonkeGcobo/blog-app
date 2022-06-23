cb5287796501c59e687512e7931b6e3176bedc02
BonkeG <madixmicro@gmail.com>
2022-06-23 19:12:13 +0200

    add validation for name

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :posts # user can have many posts
   has_many :comments, through: :post # User can have 0 or more comments in one
   has_many :likes, through: :post
-
+  validates :name, presence: true, length: { in: 3..32 }
   def first_three_posts
     posts.includes(:user).order(created_at: :DESC).limit(3)
   end
