3e850e42a6afa88594dffb4ace372bba8ba0521c
BonkeG <madixmicro@gmail.com>
2022-06-24 12:28:58 +0200

    add test for post counter if less than zero or is empty

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,12 +8,19 @@ RSpec.describe User, type: :model do
   it "user name should be present" do
     test_case = subject
     test_case.name = nil
+    test_case.PostsCounter = 3
     expect(test_case.valid?).to eq(false)
   end
 
   it 'Test if the post counter is zero or more than zero' do
     test_case = subject
     test_case.PostsCounter = nil
+    expect(test_case.valid?).to eq(false)
+  end
+
+  it 'Test if the post counter is zero or more than zero' do
+    test_case = subject
+    test_case.PostsCounter = -1
     expect(test_case.valid?).to eq(false)
   end
 end
