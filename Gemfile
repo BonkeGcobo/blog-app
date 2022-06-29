2e2bfab61cecff231835ccc798f5afd48e9ef39a
BonkeG <madixmicro@gmail.com>
2022-06-29 12:02:01 +0200

    fix linter errors

--- Gemfile
+++ Gemfile
@@ -39,7 +39,7 @@ gem 'jbuilder'
 # gem "kredis"
 
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
-gem "bcrypt", "~> 3.1.7"
+gem 'bcrypt', '~> 3.1.7'
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data'
@@ -83,6 +83,5 @@ group :development, :test do
   gem 'rails-controller-testing'
   gem 'rspec-rails', '6.0.0.rc1'
 end
-
 
 gem 'devise'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
@@ -14,12 +13,12 @@ class ApplicationController < ActionController::Base
     user.first_three_posts
   end
 
-
   protected
 
   def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :surname, :email, :password)}
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :surname, :email, :password, :current_password)}
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :surname, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :surname, :email, :password, :current_password)
+    end
   end
-
 end
