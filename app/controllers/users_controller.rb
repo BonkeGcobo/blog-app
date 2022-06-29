d545607b4bcd52c727552795405f3ac110de35ae
BonkeG <madixmicro@gmail.com>
2022-06-29 11:47:29 +0200

    authenticate user before signing in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,10 @@ class UsersController < ApplicationController
   end
 
   def show
-    @current_user = current_user
-    @user = User.find(params[:id])
+    my_id = params[:id]
+    @user_info = User.find_by(id: my_id.to_i)
+    @recent_posts = recent_posts(my_id.to_i)
+    #@current_user = current_user
+    #@user_info = User.find(params[:id])
   end
 end
