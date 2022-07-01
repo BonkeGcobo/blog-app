04a7dbbb7129bba47f16e73db6fe91766266b36f
BonkeG <madixmicro@gmail.com>
2022-07-01 12:46:50 +0200

    Fix linter errors

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,25 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe "Testing the login page", type: :feature do
-
-  describe "Test if the contents in the login page are present" do
-    it "Test if there is an email input field" do
+RSpec.describe 'Testing the login page', type: :feature do
+  describe 'Test if the contents in the login page are present' do
+    it 'Test if there is an email input field' do
       visit new_user_session_path
       expect(page).to have_selector('input[type=email]')
     end
 
-    it "Test if there is a password field" do
+    it 'Test if there is a password field' do
       visit new_user_session_path
       expect(page).to have_selector('input[type=password]')
     end
 
-    it "Test if the submit button exits" do
+    it 'Test if the submit button exits' do
       visit new_user_session_path
       expect(page).to have_selector('input[type=submit]')
     end
   end
 
-  describe "Testing the interactions of users with log in page" do
+  describe 'Testing the interactions of users with log in page' do
     before :each do
       user = User.new(name: 'Bonke', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
                       Bio: 'full-snack', email: 'bonkegcobo291195@gmail.com', password: '123456')
@@ -27,27 +26,27 @@ RSpec.describe "Testing the login page", type: :feature do
       user.save
     end
 
-    it "When I click the submit button without filling in the username and the password, I get a detailed error." do
+    it 'When I click the submit button without filling in the username and the password, I get a detailed error.' do
       visit new_user_session_path
       fill_in 'Email', with: nil
       fill_in 'Password', with: nil
-      click_button "Log in"
-      expect(page).to have_content("Invalid Email or password.")
+      click_button 'Log in'
+      expect(page).to have_content('Invalid Email or password.')
     end
 
-    it "When I click the submit button after filling in the username and the password with incorrect data, I get a detailed error." do
+    it 'When I click the submit button after filling in the username and the password with incorrect data, I get a detailed error.' do
       visit new_user_session_path
-      fill_in 'Email', with: "123@mymail.com"
-      fill_in 'Password', with: "abcdef"
-      click_button "Log in"
-      expect(page).to have_content("Invalid Email or password.")
+      fill_in 'Email', with: '123@mymail.com'
+      fill_in 'Password', with: 'abcdef'
+      click_button 'Log in'
+      expect(page).to have_content('Invalid Email or password.')
     end
 
-    it "When I click the submit button after filling in the username and the password with correct data, I am redirected to the root page." do
+    it 'When I click the submit button after filling in the username and the password with correct data, I am redirected to the root page.' do
       visit new_user_session_path
-      fill_in "Email", with: 'bonkegcobo291195@gmail.com'
-      fill_in "Password", with: '123456'
-      click_button "Log in"
+      fill_in 'Email', with: 'bonkegcobo291195@gmail.com'
+      fill_in 'Password', with: '123456'
+      click_button 'Log in'
       expect(page).to have_current_path(root_path)
     end
   end
