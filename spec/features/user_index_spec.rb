24b44c24f9d941317f9a7db1af3416a678aba157
BonkeG <madixmicro@gmail.com>
2022-07-02 11:14:15 +0200

    Write test for user_index_spec

--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -1,45 +1,52 @@
 require 'rails_helper'
 
-RSpec.describe 'Testing the user index page', type: :feature do
-  before :each do
-    user = User.new(name: 'Bonke', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
-           Bio: 'full-snack', email: 'bonkegcobo291195@gmail.com', password: '123456')
-    user.confirm
-    user.save
+RSpec.describe 'Testing user index page', type: :feature do
+  before(:each) do
+    @first = User.create(name: 'bonke', photo: 'avatar.png',
+                         Bio: 'Developer.',
+                         email: 'bonke@email.com',
+                         password: 'password', confirmed_at: Time.now,
+                         PostsCounter: 0)
 
+    @second = User.create(name: 'Madix', photo: 'avatar.png',
+                          Bio: 'Developer.', email: 'msgeme@email.com',
+                          password: 'password', confirmed_at: Time.now,
+                          PostsCounter: 0)
 
-    user_1 = User.new(name: 'Khanya', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', Bio: 'full-snack', email: '12345@gmail.com', password: 'abcdef1')
-    user_1.confirm
-    user_1.save
-    
-    user_2 = User.new(name: 'Live', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', Bio: 'full-snack', email: 'codeislife@ymail.com', password: 'bsiicy')
-    user_2.confirm
-    user_2.save       
+    @third = User.create(name: 'Djc', photo: 'avatar.png',
+                         Bio: 'Developer.', email: 'dream@email.com',
+                         password: 'password', confirmed_at: Time.now,
+                         PostsCounter: 0)
+
+    visit user_session_path
+
+    within 'form' do
+      fill_in 'Email', with: @first.email
+      fill_in 'Password', with: @first.password
+    end
+
+    click_button 'Log in'
   end
-  
-  describe "Test if can see username for all users" do
-    it "Test if can see the username of first user" do
-      visit new_user_session_path
-      fill_in "Email", with: "bonkegcobo291195@gmail.com"
-      fill_in "Password", with: "123456"
-      click_button "Log in"
-      expect(page).to have_content('Bonke')
-    end
 
-    it "Test if can see the username user_1" do
-      visit new_user_session_path
-      fill_in "Email", with: "12345@gmail.com"
-      fill_in "Password", with: "abcdef1"
-      click_button "Log in"
-      expect(page).to have_content('Khanya')
+  feature 'User Index' do
+    background { visit root_path }
+    scenario 'See all usernames' do
+      expect(page).to have_content('bonke')
+      expect(page).to have_content('Madix')
+      expect(page).to have_content('Djc')
     end
+  end
 
-    it "Test if can see the username user_1" do
-      visit new_user_session_path
-      fill_in "Email", with: "codeislife@ymail.com"
-      fill_in "Password", with: "bsiicy"
-      click_button "Log in"
-      expect(page).to have_content('Live')
-    end
+  scenario 'See profile picture for each user' do
+    expect(page.first('img')['src']).to have_content 'avatar'
+  end
+
+  scenario 'See the number of posts each user has written' do
+    expect(page).to have_content('Number of posts: 0')
+  end
+
+  scenario "When I click on a user, I am redirected to that user's show page" do
+    click_link 'bonke', match: :first
+    expect(current_path).to eq user_path(User.first.id)
   end
 end
